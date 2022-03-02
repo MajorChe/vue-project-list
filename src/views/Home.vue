452b7031720fb6386a9fe1192f2880ca06ca6679
Charit <charithnanduri03@gmail.com>
2022-03-02 11:12:31 -0500

    add delete project and complete project functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,42 @@
 <template>
-<h1>Home Page</h1>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" @update="handleUpdate"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
 
 export default {
-  name: 'Home',
-  components: { }
-}
+  name: "Home",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
+    handleUpdate(id) {
+      let p = this.projects.find((project) => {
+        return project.id === id
+      })
+      p.complete = !p.complete
+    }
+  },
+  components: { SingleProject },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((e) => console.log(e.message));
+  },
+};
 </script>
